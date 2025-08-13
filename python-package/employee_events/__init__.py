fede3757663ef4dc86fd9250c1515df2c0510f66
Divyashree Reddy <divyashreereddy23@gmail.com>
2025-08-13 03:52:15 +0000

    Changes to init file

--- python-package/employee_events/__init__.py
+++ python-package/employee_events/__init__.py
@@ -8,7 +8,7 @@ Think of it like a smart assistant that can answer questions about workers.
 from .employee import Employee
 from .team import Team
 from .query_base import QueryBase
-from .sql_execution import DatabaseMixin, database_query
+from .sql_execution import QueryMixin, database_query
 
 __version__ = "1.0.0"
 __all__ = ["Employee", "Team", "QueryBase", "DatabaseMixin", "database_query"]
